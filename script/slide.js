911c500ac10b47a5b182caffe0943fa73a4edcbd
Lucas Pinheiro Soares <129182642+soaresluccas@users.noreply.github.com>
2024-06-06 19:04:51 -0300

    Update slide.js

--- script/slide.js
+++ script/slide.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nextBtn = document.querySelector('.next_btn');
-    const prevBtn = document.querySelector('.prev_btn');
-    const cardsContainer = document.querySelector('.cards_container');
+    const nextBtn = document.querySelector('.btn_next');
+    const prevBtn = document.querySelector('.btn_prev');
+    const row = document.querySelector('#row1');
     let currentIndex = 0;
 
-    // Obter a largura de um card incluindo a margem
-    const cardWidth = document.querySelector('.card_row').offsetWidth + 20; // 20 para incluir a margem
+  
+    const cardWidth = document.querySelector('.card_row').offsetWidth + 20; 
 
     nextBtn.addEventListener('click', () => {
-        const maxIndex = cardsContainer.children.length - Math.floor(cardsContainer.offsetWidth / cardWidth);
+        const maxIndex = row.children.length - Math.floor(row.offsetWidth / cardWidth);
         if (currentIndex < maxIndex) {
             currentIndex++;
-            cardsContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            row.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
         }
     });
 
     prevBtn.addEventListener('click', () => {
         if (currentIndex > 0) {
             currentIndex--;
-            cardsContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+            row.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
         }
+        
+    });
+
+    
+    window.addEventListener('resize', () => {
+        row.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
     });
 });
+
